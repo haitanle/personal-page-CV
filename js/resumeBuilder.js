7ed8f62bc1d3d05831114680507b544d33372afa
tan <haitanle88@gmail.com>
2019-03-12 19:10:55 -0400

    create and add content dynamic using data from js object - started first with profile information. added css sytles and profile contact object

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,29 +1,40 @@
 /*
 This is empty on purpose! Your code to build the resume will go here.
  */
+
 var bio = {
 	name : 'Tan Le',
-    role : 'Software Enginner',
-    contacts : {
-          mobile: '704-451-2956',
-          email: 'haitansle@gmail.com',
-          github: 'github.com/haitanle',
-          linkedin: 'linkedin.com/in/tan-le-6b155a94',
-          location: 'Charlotte,NC'
-      }, 
-    welcomeMessage: "Hi! Welcome to my personal page! I like working at the intersection of sports and technology",
-    skills: ['Java','Python','HTML/CSS','JavaScript'],
+    role : 'Software Engineer',
+ 	mobile: '704-451-2956',
+    email: 'haitansle@gmail.com',
+    github: 'github.com/haitanle',
+    linkedin: 'linkedin.com/in/tan-le-6b155a94',
+    location: 'Charlotte,NC',
     biopic: './images/biopic.jpg',
-    display: function() {
-    	console.log('display');
-    }
+	welcomeMessage: "Hi! Welcome to my personal page! I like working at the intersection of sports and technology", 
+	skills: ['Java','Python','HTML/CSS','JavaScript']
 }
 
-//document.getElementById('header').append(HTMLheaderName+HTMLheaderRole);
+//variables used from helper.js that we will use 
+var variables = [HTMLheaderName, HTMLheaderRole, HTMLmobile, HTMLemail, HTMLgithub, HTMLblog,HTMLlocation, HTMLbioPic,
+HTMLwelcomeMsg,HTMLskills];
+
+var keyIndex = Object.keys(bio);
+
+let htmlString = '';
+for( let i = 0; i < variables.length; i++){
+	variables[i] = variables[i].replace("%data%", bio[keyIndex[i]]);
+	htmlString+= variables[i];
+}
 
 
-var formattedName = HTMLheaderName.replace("%data%", bio.name);
-var formattedRole = HTMLheaderRole.replace("%data%", bio.role);
-var formattedPic = HTMLbioPic.replace("%data%", bio.biopic);
+$("#header").append(htmlString);
 
-$("#header:last").append(formattedName).append(formattedRole).append(formattedPic);
+
+
+// variables.forEach(function(element){
+// 	$("#header").append(element);
+// })
+
+
+
